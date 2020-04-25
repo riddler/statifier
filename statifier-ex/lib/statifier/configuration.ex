784c3f9e13c0d099e15b368ef33f656dd961c4d8
JohnnyT <ubergeek3141@gmail.com>
2020-04-25 08:03:04 -0600

    Cleans up old Elixir code
    This removes most of the old Elixir code which gives us a blank slate to
    build on as we implement the official Statifier spec.
    
    Relates #16

--- statifier-ex/lib/statifier/configuration.ex
+++ statifier-ex/lib/statifier/configuration.ex
@@ -1,20 +1,2 @@
 defmodule Statifier.Configuration do
-  defstruct [:active]
-
-  def initial(state) do
-    initial_config = state |> Statifier.State.get_initial
-    %__MODULE__{active: initial_config}
-  end
-
-  #def transition!(%__MODULE__{} = configuration, transition) do
-  #  target_state = machine.states |> Enum.find(fn state -> state.id == transition.target end)
-  #end
-
-  def new(new_config) do
-    %__MODULE__{active: new_config}
-  end
-
-  def literal(%__MODULE__{} = configuration) do
-    Enum.map(configuration.active, fn state -> state.id end)
-  end
 end
